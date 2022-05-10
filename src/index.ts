74f1feb6fe77a8c7d7538ac47ac06ffde3c5c560
Tiago Dinis <tiagodinis33@gmail.com>
2022-05-10 11:56:53 +0100

    Add more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,55 @@
-
-import {Bot, BotOptions, createBot} from "mineflayer"
-import {Chance} from "chance";
+/// <reference types="jest"/>
+import { Bot, BotOptions, createBot } from "mineflayer"
+import { Chance } from "chance";
 const chance = new Chance();
+type What = "last.message"
+
 class Minepress {
+
     private bot?: Bot;
     private lastMsg?: string;
     joinServer(server: BotOptions) {
-        this.bot = createBot({
-            ...server,
-            username: chance.name().replace(" ", "").substring(0, 16)
+        return new Promise<void>((resolve, reject) => {
+            if (!this.bot) {
+                this.bot = createBot({
+                    ...server,
+                    username: chance.name().replace(" ", "").substring(0, 16)
+                })
+                this.setupBot()
+            } else {
+                this.bot.connect(server);
+            }
+            this.bot.once("spawn", () => {
+                resolve()
+            })
+            this.bot.once("error", (e) => {
+                reject()
+            })
         })
-        this.setupBot()
     }
 
-    private setupBot(){
+    private setupBot() {
         this.bot!!.on("chat", (msg) => {
             this.lastMsg = msg;
+        })
+    }
+    expect(what: What) {
+        let table = {
+            "last.message": () => {
+                return expect(this.lastMsg);
+            }
+        }
+        return table[what]();
+    }
+    quit() {
+        return new Promise<void>(resolve => {
+            this.bot?.once("end", () => {
+                resolve()
+            })
+            this.bot!!.quit()
         })
     }
 }
 // @ts-ignore
 globalThis.mi = new Minepress;
-export type {Minepress};
+export type { Minepress };
