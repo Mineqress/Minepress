b9761cd5005242b1bcd3cdc815b19ef943d46af5
Tiago Dinis <tiagodinis33@gmail.com>
2022-05-16 09:42:19 +0100

    Add some more things to asserts in expect

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@
 import { Bot, BotEvents, BotOptions, createBot } from "mineflayer"
 import { Chance } from "chance";
 import { Expect } from "@minepress/minexpect";
+import { Item } from "prismarine-item";
 
 const chance = new Chance();
-type What = "last.message"
+type What = "last.message" | "hotbar.slot" | "hand.item.type" | "hand.item.displayname" | "hand.item.amount"
 
 class Minepress {
 
@@ -49,10 +50,78 @@ class Minepress {
                     this.bot?.on("messagestr", retry);
                 }, (retry) => {
                     this.bot?.removeListener("messagestr", retry)
-                });
+                }, 10000);
+            },
+            "hotbar.slot": () => { 
+
+                const inv = this.bot!!.inventory;
+                return new Expect(this.bot?.quickBarSlot, (retry) => {
+                    inv.on("heldItemChanged", retry);
+                }, (retry) => {
+                    inv.removeListener("heldItemChanged", retry);
+                }, 5000);
+            },
+            "hand.item.type": () => {
+                const inv = this.bot!!.inventory;
+                let cb: (slot: number, _: Item, newItem: Item) => void;
+                const item = inv.slots[this.bot!!.quickBarSlot];
+                return new Expect(item? item.type: 0, (retry) => {
+                    cb = (slot, _, newItem) => {
+                        if(slot-28 == this.bot!!.quickBarSlot){
+                            retry(newItem.type)
+                        }
+                    };
+                    inv.on("updateSlot", cb)
+                }, (retry) => {
+                    inv.removeListener("updateSlot", cb);
+                }, 5000)
+            },
+
+            "hand.item.displayname": () => {
+                const inv = this.bot!!.inventory;
+                let cb: (slot: number, _: Item, newItem: Item) => void;
+                const item = inv.slots[this.bot!!.quickBarSlot];
+                return new Expect(item?.displayName, (retry) => {
+                    cb = (slot, _, newItem) => {
+                        if(slot == this.bot!!.quickBarSlot){
+                            retry(newItem.displayName)
+                        }
+                    };
+                    inv.on("updateSlot", cb)
+                }, (retry) => {
+                    inv.removeListener("updateSlot", cb);
+                }, 5000)
+            },
+            "hand.item.amount": () => {
+                const inv = this.bot!!.inventory;
+                let cb: (slot: number, _: Item, newItem: Item) => void;
+                const item = inv.slots[this.bot!!.quickBarSlot];
+                return new Expect(item? item.count: 0, (retry) => {
+                    cb = (slot, _, newItem) => {
+                        if(slot-28 == this.bot!!.quickBarSlot){
+                            retry(newItem.count)
+                        }
+                    };
+                    inv.on("updateSlot", cb)
+                }, (retry) => {
+                    inv.removeListener("updateSlot", cb);
+                }, 5000)
             }
         }
         return table[what]();
+    }
+    setHotbarSlot(slot: number) {
+        this.bot!!.setQuickBarSlot(slot)
+    }
+    clickHandItem(){
+        this.bot!!.activateItem()
+    }
+    clickOnInventory(slot: number, button: "left" | "right"){
+        let map = {
+            left: 0,
+            right: 1
+        }
+        this.bot!!.clickWindow(slot, map[button], 0)
     }
     sendMessage(message: string){
         this.bot!!.chat(message)
